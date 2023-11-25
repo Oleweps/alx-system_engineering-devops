83d1d63a08c2a19024e6c42345682999391aa88e
oleweps <weps6179@gmail.com>
2023-11-25 03:31:44 +0000

    SSH

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,17 @@
+# Create a client configuration
+
+file {'/etc/ssh/ssh_config':
+    ensure => present,
+}
+
+file_line {'Setup identity':
+    ensure => present,
+    path   => '/etc/ssh/ssh_config',
+    line   => '   IdentityFile  ~/.ssh/school',
+}
+
+file_line {'Setup password':
+    ensure => present,
+    path   => '/etc/ssh/ssh_config',
+    line   => '    PasswordAuthentication no',
+}
